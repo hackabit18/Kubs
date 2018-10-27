c3fc358fb6cd7ea2b3a76fc91b72f1cfb8a1ecf7
americast <sayan.sinha@iitkgp.ac.in>
2018-10-27 19:18:39 +0530

    Faces trained

--- cutter.py
+++ cutter.py
@@ -1,18 +1,21 @@
 import cv2
 import json
+import os
 
 f = open("data.json", "r")
 
-video_capture = cv2.VideoCapture("output.avi")
+video_capture = cv2.VideoCapture("output_wo_rect.avi")
 bgr_image = video_capture.read()[1]
 
 data = json.load(f)
-
+os.system("rm faces/*")
 # print(data)
 count = 1
 for face in data["4"]:
 	print count
 	rectDict = face["faceRectangle"]
-	crop_img = bgr_image[rectDict["top"]+2:rectDict["top"]+rectDict["height"]-2, rectDict["left"]+2:rectDict["left"]+rectDict["width"]-2]
+	crop_img = bgr_image[rectDict["top"]:rectDict["top"]+rectDict["height"], rectDict["left"]:rectDict["left"]+rectDict["width"]]
 	cv2.imwrite("faces/"+str(count)+".png", crop_img)
+
+	os.system("python track.py face_"+str(count)+" faces/"+str(count)+".png")
 	count+=1
